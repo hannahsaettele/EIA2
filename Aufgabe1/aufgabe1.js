558d66e04c6d87f08f0479821b87d3d3b90bd18c
Hannah Sättele <hannah.saettele@hs-furtwangen.de>
2021-10-13 21:43:42 +0200

    bis schritt 8

--- Aufgabe1/aufgabe1.js
+++ Aufgabe1/aufgabe1.js
@@ -1,3 +1,18 @@
-window.addEventListener("load", function () {
-});
+var Aufgabe1;
+(function (Aufgabe1) {
+    var subject = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    var verb = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    var object = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    //console.log (subject, verb, object);
+    for (var i = subject.length; i > 0; i--) {
+        //console.log (i);
+        getVerse([], [], []);
+    }
+    function getVerse(_subject, _verb, _object) {
+        return ("Alohomora");
+    }
+    //für jedes array eine eigene vorschleife 
+    //ausgehend von der länge des arrays array.length
+})(Aufgabe1 || (Aufgabe1 = {}));
+;
 //# sourceMappingURL=aufgabe1.js.map
--- Aufgabe1/aufgabe1.ts
+++ Aufgabe1/aufgabe1.ts
@@ -1,4 +1,22 @@
-window.addEventListener("load", function(): void {
-    
-});
+namespace Aufgabe1 {
+
+    let subject: string [] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let verb: string [] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let object: string [] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"]; 
+
+//console.log (subject, verb, object);
+
+    for (let i: number = subject.length ; i > 0; i-- ) {
+        //console.log (i);
+            getVerse([], [], []);
+            
+        }
+    function getVerse (_subject: string [],_verb: string [],_object: string []): string {
+        return ("Alohomora");
+
+}
+ //für jedes array eine eigene vorschleife 
+ //ausgehend von der länge des arrays array.length
+
+};
 
