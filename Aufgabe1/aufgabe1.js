a4f1764e8018dcf2ca4d35a9b6b2baf5d26e5ed6
Hannah Sättele <hannah.saettele@hs-furtwangen.de>
2021-10-14 13:01:00 +0200

    Funktion erstellt und finish

--- Aufgabe1/aufgabe1.js
+++ Aufgabe1/aufgabe1.js
@@ -1,18 +1,22 @@
 var Aufgabe1;
 (function (Aufgabe1) {
-    var subject = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-    var verb = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
-    var object = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    var subject = ["Hannah", "Theo", "Liz", "Peter", "Gustaf", "Traude"];
+    var verb = ["macht", "liebt", "studiert", "vergisst", "raucht", "lackiert"];
+    var object = ["EIA", "den Abwasch", "das ganze Studium", "die E-Zigarette", "die goldenen Ringe", "die Fingernägel"];
     //console.log (subject, verb, object);
     for (var i = subject.length; i > 0; i--) {
         //console.log (i);
-        getVerse([], [], []);
+        getVerse(subject, verb, object);
     }
     function getVerse(_subject, _verb, _object) {
+        var verse = "";
+        var getSubject = Math.floor(Math.random() * subject.length);
+        var getVerb = Math.floor(Math.random() * verb.length);
+        var getObject = Math.floor(Math.random() * object.length);
+        verse = verse + _subject.splice(getSubject, 1) + " " + _verb.splice(getVerb, 1) + " " + _object.splice(getObject, 1);
+        console.log(verse);
         return ("Alohomora");
     }
-    //für jedes array eine eigene vorschleife 
-    //ausgehend von der länge des arrays array.length
+    //console.log (getVerse);
 })(Aufgabe1 || (Aufgabe1 = {}));
-;
 //# sourceMappingURL=aufgabe1.js.map
--- Aufgabe1/aufgabe1.ts
+++ Aufgabe1/aufgabe1.ts
@@ -1,22 +1,29 @@
 namespace Aufgabe1 {
 
-    let subject: string [] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-    let verb: string [] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
-    let object: string [] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"]; 
+    let subject: string [] = ["Hannah", "Theo", "Liz", "Peter", "Gustaf", "Traude"];
+    let verb: string [] = ["macht", "liebt", "studiert", "vergisst", "raucht", "lackiert"];
+    let object: string [] = ["EIA", "den Abwasch", "das ganze Studium", "die E-Zigarette", "die goldenen Ringe", "die Fingernägel"]; 
 
 //console.log (subject, verb, object);
 
     for (let i: number = subject.length ; i > 0; i-- ) {
         //console.log (i);
-            getVerse([], [], []);
+            getVerse(subject, verb, object);
             
         }
     function getVerse (_subject: string [],_verb: string [],_object: string []): string {
+       let verse: string = "";
+       let getSubject: number = Math.floor(Math.random() * subject.length); 
+       let getVerb: number = Math.floor(Math.random() * verb.length);
+       let getObject: number = Math.floor(Math.random() * object.length);
+
+       verse = verse + _subject.splice(getSubject, 1) + " " + _verb.splice(getVerb, 1) + " " + _object.splice(getObject, 1);
+        console.log(verse);
+
         return ("Alohomora");
-
+        
 }
- //für jedes array eine eigene vorschleife 
- //ausgehend von der länge des arrays array.length
-
-};
+    //console.log (getVerse);
+    
+}
 
